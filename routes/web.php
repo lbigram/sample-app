b5f56f3c619f026eb6590abf163b22b673116bdd
LB <laura.bigram@gmail.com>
2024-03-17 16:38:03 -0400

    routes

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/users', 'UserController@index');
+Route::post('/users', 'UserController@store');
+Route::get('/users/{id}', 'UserController@show');
+Route::put('/users/{id}', 'UserController@update');
+Route::delete('/users/{id}', 'UserController@destroy');
